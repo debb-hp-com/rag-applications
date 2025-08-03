17840a4fea2ae0b866d33192473cb6b801b97242
Deb Briggs <dbriggs@proofpoint.com>
2025-08-03 13:02:48 -0700

    with these changes, the app run though there may still be bugs

--- src/rag_ui.py
+++ src/rag_ui.py
@@ -41,7 +41,7 @@ st.set_page_config(page_title="Huberman Labs",
 
 ICON_DIR = './app_assets/'
 reader_model_name = 'gpt-4o-mini'
-collection_name = None # set to None to use the sidebar selectbox
+# collection_name = None # set to None to use the sidebar selectbox
 data_path = '../data/huberman_labs.json'
 embedding_model_path = '../models/allminilm-finetuned-256/'
 ###################################
@@ -70,6 +70,7 @@ encoding = get_encoding("cl100k_base")
 
 ## Display properties
 display_properties = None
+verbosity_options = ['low', 'medium', 'high']
 
 ## Data
 data = load_data(data_path)
@@ -92,12 +93,11 @@ def main(retriever: WeaviateWCS):
     with st.sidebar:
         collection_name = st.selectbox( 'Collection Name:',options=available_collections, index=None,placeholder='Select Collection Name')
         guest_input = st.selectbox('Select Guest', options=guest_list,index=None, placeholder='Select Guest')
-        alpha_input = None
-        retrieval_limit = None
-        reranker_topk = None
-        temperature_input = None
-        verbosity = None
-        
+        alpha_input = st.slider('Alpha (Hybrid Search Weighting)', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+        retrieval_limit = st.number_input('Retrieval Limit', min_value=1, value=5)
+        reranker_topk = st.number_input('Reranker Top K', min_value=1, value=5)
+        temperature_input = st.slider('Temperature', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+        verbosity = st.selectbox("Verbosity:", options=range(len(verbosity_options)), format_func=lambda i: verbosity_options[i], index=0)
     # retriever.return_properties.append('expanded_content')
 
     ##############################
@@ -110,81 +110,81 @@ def main(retriever: WeaviateWCS):
     with col1:
         query = st.text_input('Enter your question: ')
         st.write('\n\n\n\n\n')
-        if query:
-            st.write('This app is not currently functioning as intended. Uncomment lines 104-172 to enable Q&A functionality.')
+     #   if query:
+     #       st.write('This app is not currently functioning as intended. Uncomment lines 104-172 to enable Q&A functionality.')
 
     ########################
     ##### SEARCH + LLM #####
     ########################
-    # if query and not collection_name:
-    #     raise ValueError('Please first select a collection name')
-    # if query:
-    #     # make hybrid call to weaviate
-    #     guest_filter = Filter.by_property(name='guest').equal(guest_input) if guest_input else None
+    if query and not collection_name:
+        raise ValueError('Please first select a collection name')
+    if query:
+         # make hybrid call to weaviate
+         guest_filter = Filter.by_property(name='guest').equal(guest_input) if guest_input else None
+         display_properties = [prop.name for prop in retriever.show_collection_properties(collection_name)]
+         hybrid_response = retriever.hybrid_search(query, collection_name, alpha=alpha_input, limit=5, return_properties=display_properties,
+                                                  filter=guest_filter)
+         ranked_response = reranker.rerank(hybrid_response, query, apply_sigmoid=True)       
+         logger.info(f'# RANKED RESULTS: {len(ranked_response)}')   
 
-    #     hybrid_response = None
+         token_threshold = 2500 # generally allows for 3-5 results of chunk_size 256
+         content_field = 'content'
 
-    #     ranked_response = None        
-    #     logger.info(f'# RANKED RESULTS: {len(ranked_response)}')   
-
-    #     token_threshold = 2500 # generally allows for 3-5 results of chunk_size 256
-    #     content_field = 'content'
-
-    #     # validate token count is below threshold
-    #     valid_response = validate_token_threshold(  ranked_response, 
-    #                                                 query=query,
-    #                                                 system_message=huberman_system_message,
-    #                                                 tokenizer=encoding,# variable from ENCODING,
-    #                                                 llm_verbosity_level=verbosity,
-    #                                                 token_threshold=token_threshold, 
-    #                                                 content_field=content_field,
-    #                                                 verbose=True)
-    #     logger.info(f'# VALID RESULTS: {len(valid_response)}')
-    #     #set to False to skip LLM call
-    #     make_llm_call = True
-    #     # prep for streaming response
-    #     with st.spinner('Generating Response...'):
-    #         st.markdown("----")                
-    #         # generate LLM prompt
-    #         prompt = generate_prompt_series(query=query, results=valid_response, verbosity_level=verbosity)
-    #         if make_llm_call:
-    #             with st.chat_message('Huberman Labs', avatar=f'{ICON_DIR}/huberman_logo.png'):
-    #                 stream_obj = stream_chat(llm, prompt, max_tokens=250, temperature=temperature_input)
-    #                 st.write_stream(stream_obj) # https://docs.streamlit.io/develop/api-reference/write-magic/st.write_stream
+         # validate token count is below threshold
+         valid_response = validate_token_threshold(  ranked_response, 
+                                                     query=query,
+                                                     system_message=huberman_system_message,
+                                                     tokenizer=encoding,# variable from ENCODING,
+                                                     llm_verbosity_level=verbosity,
+                                                     token_threshold=token_threshold, 
+                                                     content_field=content_field,
+                                                     verbose=True)
+         logger.info(f'# VALID RESULTS: {len(valid_response)}')
+         #set to False to skip LLM call
+         make_llm_call = True
+         # prep for streaming response
+         with st.spinner('Generating Response...'):
+             st.markdown("----")                
+             # generate LLM prompt
+             prompt = generate_prompt_series(query=query, results=valid_response, verbosity_level=verbosity)
+             if make_llm_call:
+                 with st.chat_message('Huberman Labs', avatar=f'{ICON_DIR}/huberman_logo.png'):
+                     stream_obj = stream_chat(llm, prompt, max_tokens=250, temperature=temperature_input)
+                     st.write_stream(stream_obj) # https://docs.streamlit.io/develop/api-reference/write-magic/st.write_stream
             
-    #         # need to pull out the completion for cost calculation
-    #         string_completion = ' '.join([c for c in stream_obj])
-    #         call_cost = completion_cost(completion=string_completion, 
-    #                                     model=turbo, 
-    #                                     prompt=huberman_system_message + ' ' + prompt,
-    #                                     call_type='completion')
-    #         st.session_state['cost_counter'] += call_cost
-    #         logger.info(f'TOTAL SESSION COST: {st.session_state["cost_counter"]}')
+             # need to pull out the completion for cost calculation
+             string_completion = ' '.join([c for c in stream_obj])
+             call_cost = completion_cost(completion=string_completion, 
+                                         model=mini, 
+                                         prompt=huberman_system_message + ' ' + prompt,
+                                         call_type='completion')
+             st.session_state['cost_counter'] += call_cost
+             logger.info(f'TOTAL SESSION COST: {st.session_state["cost_counter"]}')
 
     # ##################
     # # SEARCH DISPLAY #
     # ##################
-    #         st.subheader("Search Results")
-    #         for i, hit in enumerate(valid_response):
-    #             col1, col2 = st.columns([7, 3], gap='large')
-    #             episode_url = hit['episode_url']
-    #             title = hit['title']
-    #             show_length = hit['length_seconds']
-    #             time_string = convert_seconds(show_length) # convert show_length to readable time string
-    #             with col1:
-    #                 st.write( search_result(i=i, 
-    #                                         url=episode_url,
-    #                                         guest=hit['guest'],
-    #                                         title=title,
-    #                                         content=ranked_response[i]['content'], 
-    #                                         length=time_string),
-    #                                         unsafe_allow_html=True)
-    #                 st.write('\n\n')
+             st.subheader("Search Results")
+             for i, hit in enumerate(valid_response):
+                 col1, col2 = st.columns([7, 3], gap='large')
+                 episode_url = hit['episode_url']
+                 title = hit['title']
+                 show_length = hit['length_seconds']
+                 time_string = convert_seconds(show_length) # convert show_length to readable time string
+                 with col1:
+                     st.write( search_result(i=i, 
+                                             url=episode_url,
+                                             guest=hit['guest'],
+                                             title=title,
+                                             content=ranked_response[i]['content'], 
+                                             length=time_string),
+                                             unsafe_allow_html=True)
+                     st.write('\n\n')
 
-    #             with col2:
-    #                 image = hit['thumbnail_url']
-    #                 st.image(image, caption=title.split('|')[0], width=200, use_column_width=False)
-    #                 st.markdown(f'<p style="text-align": right;"><b>Guest: {hit["guest"]}</b>', unsafe_allow_html=True)
+                 with col2:
+                     image = hit['thumbnail_url']
+                     st.image(image, caption=title.split('|')[0], width=200, use_container_width=False)
+                     st.markdown(f'<p style="text-align": right;"><b>Guest: {hit["guest"]}</b>', unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main(retriever)
